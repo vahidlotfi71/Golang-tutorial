4323bda4487bdb911b412e693c2201dc050176cb
vahid lotfi <v.lotfi71@gmail.com>
2024-10-22 21:43:46 +0330

    S9 9_1.go

--- S9/test.go
+++ S9/test.go
@@ -1,69 +1,63 @@
 package main
 
-import "fmt"
-
-const (
-	SalaryBase            = 10000
-	ExtraHourRateFullTime = 10
-	ExtraHourRatePartTime = 10
-	TaxRate               = 0.10
+import (
+	"encoding/json"
+	"fmt"
 )
+
+type Product struct {
+	Name   string
+	Color  string
+	lenght int
+	wight  int
+	Weight int
+	Price  int
+	Brand  string
+	MedIn  string
+}
+
+type ElectronicProduct struct {
+	Product
+	Ram                    int
+	Cpu                    int
+	ScreenSize             float64
+	OperatingSystem        string
+	OperatingSystemVersion string
+}
+
+type Mobile struct {
+	ElectronicProduct
+	SimCardCapecity int
+	SimCardType     string
+	NetworkType     string
+	CameraCount     int
+}
+
+type Laptop struct {
+	ElectronicProduct
+	UsbPortCount int
+	KeyboardType string
+	HasCdRom     bool
+}
 
 func main() {
 
-	fullTimeEmployees := []FullTimeEmployee{
-		{Id: 1, NationalCode: "1234567890", FullName: "Pejman Rezaee", ExtraHours: 40},
-		{Id: 2, NationalCode: "4836524125", FullName: "Maryam Hosseini", ExtraHours: 120},
-	}
+	phone := new(Mobile)
+	phone.Name = "sumsung 1212"
+	phone.Brand = "sumsung"
+	phone.Color = "Red"
+	phone.Ram = 12
+	phone.OperatingSystemVersion = "Android"
+	phone.OperatingSystemVersion = "12"
+	phone.CameraCount = 3
+	phone.NetworkType = "5G"
+	phone.SimCardType = "Nano"
+	phone.SimCardCapecity = 2
 
-	partTimeEmployees := []PartTimeEmployee{
-		{Id: 3, NationalCode: "6563453455", FullName: "Milad Hassani", PartTimeHours: 100},
-		{Id: 4, NationalCode: "5435435435", FullName: "Maryam Rezaee", PartTimeHours: 87},
-	}
+	fmt.Printf("mobile : %+v\n", phone)
 
-	var employye FullTimeEmployee = fullTimeEmployees[0]
+	mobileJson , _ := json.Marshal(phone)
 
-	var employee1 PartTimeEmployee = partTimeEmployees[0]
+	println(string(mobileJson))
 
-	salary , tax := employye.SalaryCalculate(employye.ExtraHours)
-	salary1 , tax1 := employee1.SalaryCalculate(employee1.PartTimeHours)
-
-	fmt.Println("Employee : %v , salary : %f , tax : %f", employye , salary , tax)
-	fmt.Println("Employee : %v , salary : %f , tax : %f", employee1 , salary1 , tax1)
-
-	
-
-
-
-}
-
-type Employee interface{
-	SalaryCalculate(hours float64)  (salary , tax float64)
-}
-
-type FullTimeEmployee struct {
-	Id           int
-	NationalCode string
-	FullName     string
-	Type         string
-	ExtraHours   float64
-}
-
-type PartTimeEmployee struct {
-	Id            int
-	NationalCode  string
-	FullName      string
-	PartTimeHours float64
-}
-
-func (employee FullTimeEmployee) SalaryCalculate(extraHours float64) (salary, tax float64) {
-	salary = SalaryBase + extraHours*ExtraHourRateFullTime
-	tax = salary * TaxRate
-	return
-}
-
-func (employee PartTimeEmployee) SalaryCalculate(extraHours float64) (salary, tax float64) {
-	salary = ExtraHourRatePartTime * extraHours
-	tax = salary * TaxRate
-	return
 }
