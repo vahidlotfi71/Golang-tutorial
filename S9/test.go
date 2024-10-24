305007f3080f9e4b7d8c2fbc27df4bc99aeea9a3
vahid lotfi <v.lotfi71@gmail.com>
2024-10-24 15:32:58 +0330

    s9 11_1.go

--- S9/test.go
+++ S9/test.go
@@ -1,67 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type Person struct {
-	Name      string
-	Age       int
-	Family    string
+	Name      string  `json:"first_name"`
+	Age       int	   
+	Family    string  `json:"last_name"`
 	Gender    string
 	Height    int
 	Weight    int
 	HairColor string
 }
 
-type PersonOptions func(*Person)
-
 func main() {
-
-	person1 := NewPerson(SetName("John"), SetAge(32), SetFamily("lotfi"))
+	person1 := Person{Name: "John", Age: 32, Family: "lotfi", Gender: "male", Height: 173, Weight: 70, HairColor: "Black"}
 
 	fmt.Println(person1)
 
-}
+	personJson , _ := json.MarshalIndent(person1, "", " ")
 
-func NewPerson(options ...PersonOptions) *Person {
-	person := &Person{}
-	for _, option := range options {
-		option(person)
-	}
-	return person
-}
-
-func SetName(name string) PersonOptions {
-	return func(person *Person) {
-		person.Name = name
-	}
-}
-
-func SetAge(age int) PersonOptions {
-	return func(person *Person) {
-		person.Age = age
-	}
-}
-
-func SetFamily(family string) PersonOptions {
-	return func(person *Person) {
-		person.Family = family
-	}
-}
-
-func SetGender(gender string) PersonOptions {
-	return func(person *Person) {
-		person.Gender = gender
-	}
-}
-
-func SetHeight(height int) PersonOptions {
-	return func(person *Person) {
-		person.Height = height
-	}
-}
-
-func SetWeight(weight int) PersonOptions {
-	return func(person *Person) {
-		person.Weight = weight
-	}
+	fmt.Println(string(personJson))
 }
