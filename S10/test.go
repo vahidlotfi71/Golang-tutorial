32a7f0dd2e07d77fa7b551e63c31400127576034
vahid lotfi <v.lotfi71@gmail.com>
2024-10-25 11:22:26 +0330

    finished p10 s4

---
+++ S10/test.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Eat()
+	Sleep()
+	Walk()
+}
+
+type Human interface {
+	Animal
+	Speak()
+	Think()
+}
+
+type Employee struct {
+	Human
+	Name string
+	Age  int
+}
+
+type Cat struct {
+	Animal
+	Name string
+}
+
+func main() {
+
+	employee  := &Employee{Name :"John", Age:32}
+	cat := &Cat{Name : "sjdhf"}
+
+	var human Human = employee
+	var animal Animal =  cat
+
+	human.Eat()
+	human.Sleep()
+	human.Walk()
+	human.Speak()
+	human.Think()
+
+	animal.Eat()
+	animal.Sleep()
+	animal.Walk()
+}
+
+
+func (cat *Cat) Eat() {
+	fmt.Println("Cat is Eating")
+}
+
+func (cat *Cat) Sleep(){
+	fmt.Println("Cat is sleeping")
+}
+
+func (cat *Cat) Walk() {
+	fmt.Println("Cat is walking")
+}
+
+func (employee *Employee) Speak() {
+	fmt.Println("Employee is speaking")
+}
+
+func (employee *Employee) Think() {
+	fmt.Println("Employee is thinking")
+}
+
+func (employee *Employee) Eat() {
+	fmt.Println("Employee is eating")
+}
+
+func (employee *Employee) Sleep() {
+	fmt.Println("Employee is sleeping")
+}
+
+func (employee *Employee) Walk() {
+	fmt.Println("Employee is walking")
+}
+
